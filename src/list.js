28bdc240c8c9be928f1cc27407daaffce9f6760d
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-09-27 18:18:25 -0300

    Add unique

--- src/list.js
+++ src/list.js
@@ -12,3 +12,17 @@ const removeIndex = (list, ...indexes) => {
 }
 
 exports.removeIndex = removeIndex
+
+/**
+ * Return an array with just unique values.
+ * @param {Iterable.<T>} list
+ * @template T
+ * @returns {Array.<T>}
+ */
+const unique = (list) => {
+  const set = new Set([...list])
+  const unique = [...set]
+  return unique
+}
+
+exports.unique = unique
